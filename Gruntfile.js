a6dabd00c2a895773fc87c0e3f2d848279e125a6
Mengzhu <mzga@ucdavis.edu>
2022-12-21 10:15:11 -0800

    changes and readme

---
+++ Gruntfile.js
@@ -0,0 +1,173 @@
+'use strict';
+
+module.exports = function(grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt, {
+        useminPrepare: 'grunt-usemin'
+    });
+    const sass = require('node-sass');
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            },
+            options: {
+                implementation: sass,
+                sourceMap: true
+            }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./"
+                    }
+                }
+            }
+        },
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['img/*.{png,jpg,gif}'],
+                    dest: 'dist'
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['index.html'],
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function(context, block) {
+                                var generated = context.options.generated;
+                                generated.options = {
+                                    keepSpecialCommments: 0,
+                                    rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                seperator: ';'
+            },
+            dist: {}
+        },
+        uglify: {
+            dist: {}
+        },
+        cssmin: {
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            release: {
+                files: [{
+                    src: [
+                        'dist/js/*.js',
+                        'dist/css/*.css'
+                    ]
+                }]
+            }
+
+        },
+        usemin: {
+            html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css', 'dist/js']
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'dist/index.html',
+                    'dist/aboutus.html': 'dist/aboutus.html',
+                    'dist/index.html': 'dist/index.html'
+                }
+            }
+        }
+
+
+    });
+
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', [
+        'clean',
+        'copy',
+        'imagemin',
+        'useminPrepare',
+        'concat',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'htmlmin'
+    ])
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+The repo is for the project of the course ["Front-End Web UI Frameworks and Tools: Bootstrap 4"](https://www.coursera.org/learn/bootstrap-4).
+see cirtificate [here](https://coursera.org/share/977ebeb2e272325660cb97e9d7d1b38d).
+
+A restaurant web page is development with HTML, CSS, and JavaScript, and deployed with npm scripts and Grunt.
+run command:
+```
+npm start
+```
